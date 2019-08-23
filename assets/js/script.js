c5de1190c75336a5ca50110b42ce41bc51fafe68
taleknight <reinaldo.ferreira@dce.upfb.br>
2019-08-23 16:05:31 -0300

    implementando JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+let email = 'reinaldo@hotmail.com';
+
+email = 'juca@htomail.com'; /*altera o email definido*/
+
+console.log(email);
+
+console.log(`O seu E-mail é: ${email}`); /* email seguido de CRÁSE*/
+
+
+document.getElementById('btn-submit').addEventListener('click', e =>{
+
+    console.log('O botão foi clicado!');
+
+});
+
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+
+    console.log('O mouse está sobre o formulário.');
+
+});
+
+document.querySelector('#form-login').addEventListener('mouseleave', e =>{
+
+    console.log('O mouse está fora do formulário.');
+
+});
+
+document.querySelector('#form-login').addEventListener('submit', e =>{
+
+    e.preventDefault();
+    console.log('Formulário enviado! Aqui vai o Ajax.');
+
+});
